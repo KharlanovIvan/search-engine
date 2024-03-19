fb8f9ed0418c9ab9db0f05b046b70def94ea8f0c
Kharlanov Ivan <kharlanov.ivan.a@yandex.ru>
2024-03-19 19:05:20 +0300

    Первый коммит. Подключена библиотека nlohmann_json.

---
+++ build/CMakeFiles/Search_Engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Search_Engine.dir/search_engine.cpp.obj"
+  "CMakeFiles/Search_Engine.dir/search_engine.cpp.obj.d"
+  "Search_Engine.exe"
+  "Search_Engine.exe.manifest"
+  "Search_Engine.pdb"
+  "libSearch_Engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Search_Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
