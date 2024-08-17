9b6662b286ef07171d9191f0580293c3bb0e9a75
Ivan Kharlanov <kharlanov.ivan.a@yandex.ru>
2024-08-17 15:34:19 +0300

    The project structure has been changed. Added a method fetchContent for the nlohman library.

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,56 @@
 
 
 
+struct Entry {
+    size_t doc_id; 
+    size_t count;
 
-
+    // Данный оператор необходим для проведения тестовых сценариев
+    bool operator ==(const Entry& other) const {
+    return (doc_id == other.doc_id &&
+    count == other.count);
+    }
+};
 
 
 class InvertedIndex {
 
-    public:
+    std::vector<std::string> docs; // список содержимого документов
+    std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный словарь
+public:
+    InvertedIndex() = default;
+/**
+* Обновить или заполнить базу документов, по которой будем совершать
+поиск
+* @param texts_input содержимое документов
+*/
+    void UpdateDocumentBase(std::vector<std::string> input_docs);
+/*
+* Метод определяет количество вхождений слова word в загруженной базе
+документов
+* @param word слово, частоту вхождений которого необходимо определить
+* @return возвращает подготовленный список с частотой слов
+*/
+    std::vector<Entry> GetWordCount(const std::string& word);
 
 };
 
 
+/*
+class InvertedIndex {
+    std::vector<std::string> docs; // - коллекция для хранения текстов документов, в которой номер 
+                                   //элемента в векторе определяет doc_id для формирования результата запроса        
+    std::map<std::string, std::vector<Entry>> freq_dictionary; //частотный словарь — это коллекция для хранения частоты слов, встречаемых в тексте.
+public:
 
+ void UpdateDocumentBase () {
+
+ }
+
+};
+
+
+*/
 
 /**
 * Класс для работы с JSON-файлами
